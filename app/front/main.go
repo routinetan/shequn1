18ced0d909f24570333906b7d0cb55337cf30e59
tfcwolf123 <13627493403@163.com>
2022-11-13 18:44:00 +0800

    增加迁移

--- app/front/main.go
+++ app/front/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gogf/gf/os/gspath"
 	"shequn1/foundation/engine"
 	"shequn1/foundation/server"
 	"shequn1/internal/front"
@@ -8,7 +10,8 @@ import (
 
 func main() {
 	server.Mode = "front"
-
+	path, _ := gspath.Search("/www/shequn1/web/", "/front/")
+	fmt.Println(path)
 	// pages.New(&Table).List(ctx)
 	server.Run(func(engine engine.Engine) { engine.Run(front.GetRouter) })
 }
