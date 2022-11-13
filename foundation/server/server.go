66189a3aa8ee2dabaa4cf365502950ebc2b3b381
tfcwolf123 <13627493403@163.com>
2022-11-14 00:04:27 +0800

    部署

--- foundation/server/server.go
+++ foundation/server/server.go
@@ -79,12 +79,12 @@ func Run(router func(engine engine.Engine)) {
 	//if swagHandler != nil && gin.Mode() != gin.ReleaseMode {
 	//	engine.GET("/doc/*any", swagHandler)
 	//}
-	createServer(engineInst.GetHandler()).ListenAndServe()
-	//_ = gracehttp.ServeWithOptions([]*http.Server{createServer(engine)}, gracehttp.PreStartProcess(func() error {
-	//	app.Logger().WithField("log_type", "foundation.server.server").Println("unlock pid")
-	//	lock.UnLock()
-	//	return nil
-	//}))
+	//createServer(engineInst.GetHandler()).ListenAndServe()
+	_ = gracehttp.ServeWithOptions([]*http.Server{createServer(engine)}, gracehttp.PreStartProcess(func() error {
+		app.Logger().WithField("log_type", "foundation.server.server").Println("unlock pid")
+		lock.UnLock()
+		return nil
+	}))
 }
 
 func createServer(router http.Handler) *http.Server {
@@ -103,15 +103,86 @@ func createServer(router http.Handler) *http.Server {
 }
 
 // 对启动进程记录进程id
-//func createPid() *app.Flock {
-//pidLock, pidLockErr := app.FLock(pidFile)
-//if pidLockErr != nil {
-//	app.Logger().WithField("log_type", "foundation.server.server").Fatalln("createPid: lock error", pidLockErr)
-//}
+func createPid() *app.Flock {
+	pidLock, pidLockErr := app.FLock(pidFile)
+	if pidLockErr != nil {
+		app.Logger().WithField("log_type", "foundation.server.server").Fatalln("createPid: lock error", pidLockErr)
+	}
 
-//err := pidLock.WriteTo(fmt.Sprintf(`%d`, os.Getpid()))
-//if err != nil {
-//	app.Logger().WithField("log_type", "foundation.server.server").Fatalln("write error: ", err)
-//}
-//return nil
-//}
+	err := pidLock.WriteTo(fmt.Sprintf(`%d`, os.Getpid()))
+	if err != nil {
+		app.Logger().WithField("log_type", "foundation.server.server").Fatalln("write error: ", err)
+	}
+	return pidLock
+}
+
+// 自定义的GIN日志处理中间件
+// 可能在终端输出时看起来比较的难看
+func logger(ctx *gin.Context) {
+	start := time.Now()
+	path := ctx.Request.URL.Path
+	raw := ctx.Request.URL.RawQuery
+
+	ctx.Next()
+
+	if raw != "" {
+		path = path + "?" + raw
+	}
+
+	var params = make(logrus.Fields)
+	params["latency"] = time.Now().Sub(start)
+	params["path"] = path
+	params["method"] = ctx.Request.Method
+	params["status"] = ctx.Writer.Status()
+	params["body_size"] = ctx.Writer.Size()
+	params["client_ip"] = ctx.ClientIP()
+	params["user_agent"] = ctx.Request.UserAgent()
+	params["log_type"] = "foundation.server.server"
+	if !gin.IsDebugging() {
+		// 在正式环境将上下文传递的变量也进行记录, 方便分析
+		params["keys"] = ctx.Keys
+	}
+	app.Logger().WithFields(params).Info("request success, status is ", ctx.Writer.Status(), ", client ip is ", ctx.ClientIP())
+}
+
+func recovery(ctx *gin.Context) {
+	defer func() {
+		if err := recover(); err != nil {
+			var brokenPipe bool
+			if ne, ok := err.(*net.OpError); ok {
+				if se, ok := ne.Err.(*os.SyscallError); ok {
+					if strings.Contains(strings.ToLower(se.Error()), "broken pipe") || strings.Contains(strings.ToLower(se.Error()), "connection reset by peer") {
+						brokenPipe = true
+					}
+				}
+			}
+			stack := app.Stack(3)
+			httpRequest, _ := httputil.DumpRequest(ctx.Request, false)
+
+			if gin.IsDebugging() {
+				app.Logger().WithField("log_type", "foundation.server.server").Error(string(httpRequest))
+				var errors = make([]logrus.Fields, 0)
+				for i := 0; i < len(stack); i++ {
+					errors = append(errors, logrus.Fields{
+						"func":   stack[i]["func"],
+						"source": stack[i]["source"],
+						"file":   fmt.Sprintf("%s:%d", stack[i]["file"], stack[i]["line"]),
+					})
+				}
+				app.Logger().WithField("log_type", "foundation.server.server").WithField("stack", errors).Error(err)
+				ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"stack": errors, "message": err})
+			} else {
+				app.Logger().WithField("log_type", "foundation.server.server").
+					WithField("stack", stack).WithField("request", string(httpRequest)).Error()
+			}
+
+			if brokenPipe {
+				_ = ctx.Error(err.(error))
+				ctx.Abort()
+			} else {
+				ctx.AbortWithStatus(http.StatusInternalServerError)
+			}
+		}
+	}()
+	ctx.Next()
+}
